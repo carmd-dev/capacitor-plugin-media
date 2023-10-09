26b2a10bd25c312981a97ba14abc200718ebc531
Viet Huynh <vieth@carmd.com>
2023-10-10 06:25:49 +0700

    added dist folder

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,205 @@
+export interface MediaPlugin {
+    /**
+      * Get filtered media from camera roll (pictures only currently). iOS only.
+      *
+      * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/GetMedias.tsx)
+      */
+    getMedias(options?: MediaFetchOptions): Promise<MediaResponse>;
+    /**
+      * Get list of albums.
+      *
+      * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/GetAlbums.tsx)
+      */
+    getAlbums(): Promise<MediaAlbumResponse>;
+    /**
+     * Saves a photo to the camera roll.
+     *
+     * On Android and iOS, this supports web URLs, base64 encoded images
+     * (e.g. data:image/jpeg;base64,...), and local files.
+     * On Android, all image formats supported by the user's photo viewer are supported.
+     *
+     * On iOS, [all image formats supported by SDWebImage are supported.](https://github.com/SDWebImage/SDWebImage#supported-image-formats)
+     * All images on iOS are converted to PNG for system compatability.
+     *
+     * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/SaveMedia.tsx)
+     */
+    savePhoto(options?: MediaSaveOptions): Promise<PhotoResponse>;
+    /**
+     * Saves a video to the camera roll.
+     *
+     * On Android and iOS, this supports web URLs, base64 encoded videos
+     * (e.g. data:image/mp4;base64,...), and local files.
+     * On Android, all video formats supported by the user's photo viewer are supported.
+     * On iOS, the supported formats are based on whatever iOS supports at the time.
+     *
+     * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/SaveMedia.tsx)
+     */
+    saveVideo(options?: MediaSaveOptions): Promise<PhotoResponse>;
+    /**
+     * Saves an animated GIF to the camera roll.
+     *
+     * On Android and iOS, this supports web URLs, base64 encoded GIFs
+     * (e.g. data:image/gif;base64,...), and local files.
+     * This only supports GIF files specifically.
+     *
+     * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/SaveMedia.tsx)
+     */
+    saveGif(options?: MediaSaveOptions): Promise<PhotoResponse>;
+    /**
+     * Creates an album.
+     *
+     * [Code Examples](https://github.com/capacitor-community/media/blob/master/example/src/components/CreateDemoAlbum.tsx)
+     */
+    createAlbum(options: MediaAlbumCreate): Promise<void>;
+}
+export interface MediaSaveOptions {
+    /**
+     * Web URL, base64 encoded URI, or local file path to save.
+     */
+    path: string;
+    /**
+     * Album identifier from getAlbums().
+     * Since 5.0, identifier is used on both Android and iOS.
+     * Identifier is required on Android but not on iOS.
+     * On iOS 14+, if the identifier is not specified and no permissions
+     * have been requested yet, add-only permissions will be requested instead
+     * of full permissions (assuming NSPhotoLibraryAddUsageDescription
+     * is in Info.plist).
+     */
+    albumIdentifier?: string;
+    /**
+     * File name to save the image as in the album.
+     * Do not include extension. Android only.
+     */
+    fileName?: string;
+}
+export interface MediaFetchOptions {
+    /**
+     * The number of photos to fetch, sorted by last created date descending
+     */
+    quantity?: number;
+    /**
+     * The width of thumbnail to return
+     */
+    thumbnailWidth?: number;
+    /**
+     * The height of thumbnail to return
+     */
+    thumbnailHeight?: number;
+    /**
+     * The quality of thumbnail to return as JPEG (0-100)
+     */
+    thumbnailQuality?: number;
+    /**
+     * Which types of assets to return. Only photos supported currently.
+     */
+    types?: "photos";
+    /**
+     * Which album identifier to query in (get identifier with getAlbums())
+     */
+    albumIdentifier?: string;
+    /**
+     * Sort order of returned assets by field and ascending/descending
+     */
+    sort?: MediaField | MediaSort[];
+}
+export interface MediaSort {
+    key: MediaField;
+    ascending: boolean;
+}
+/**
+ * Attributes to sort media by.
+ *
+ * [iOS Source](https://developer.apple.com/documentation/photokit/phfetchoptions)
+ */
+export declare type MediaField = 'mediaType' | 'mediaSubtypes' | 'sourceType' | 'pixelWidth' | 'pixelHeight' | 'creationDate' | 'modificationDate' | 'isFavorite' | 'burstIdentifier';
+export interface MediaResponse {
+    medias: MediaAsset[];
+}
+export interface MediaAsset {
+    /**
+     * Platform-specific identifier
+     */
+    identifier: string;
+    /**
+     * Data for a photo asset as a base64 encoded string (JPEG only supported)
+     */
+    data: string;
+    /**
+     * ISO date string for creation date of asset
+     */
+    creationDate: string;
+    /**
+     * Full width of original asset
+     */
+    fullWidth: number;
+    /**
+     * Full height of original asset
+     */
+    fullHeight: number;
+    /**
+     * Width of thumbnail preview
+     */
+    thumbnailWidth: number;
+    /**
+     * Height of thumbnail preview
+     */
+    thumbnailHeight: number;
+    /**
+     * Location metadata for the asset
+     */
+    location: MediaLocation;
+}
+export interface MediaLocation {
+    /**
+     * GPS latitude image was taken at
+     */
+    latitude: number;
+    /**
+     * GPS longitude image was taken at
+     */
+    longitude: number;
+    /**
+     * Heading of user at time image was taken
+     */
+    heading: number;
+    /**
+     * Altitude of user at time image was taken
+     */
+    altitude: number;
+    /**
+     * Speed of user at time image was taken
+     */
+    speed: number;
+}
+export interface MediaAlbumResponse {
+    albums: MediaAlbum[];
+}
+export interface MediaAlbum {
+    identifier: string;
+    name: string;
+    type?: MediaAlbumType;
+}
+export declare enum MediaAlbumType {
+    /**
+     * Album is a "smart" album (such as Favorites or Recently Added)
+     */
+    Smart = "smart",
+    /**
+     * Album is a cloud-shared album
+     */
+    Shared = "shared",
+    /**
+     * Album is a user-created album
+     */
+    User = "user"
+}
+export interface MediaAlbumCreate {
+    name: string;
+}
+export interface PhotoResponse {
+    /**
+     * Available on Android only.
+     */
+    filePath?: string;
+}
